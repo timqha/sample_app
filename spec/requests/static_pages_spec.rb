d2a22bb7f720c08dfda836aae9131706bf484f30
Pro_solvo <timqha@gmail.com>
2015-02-20 16:38:38 +0200

    add Guardfile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+
 describe "StaticPages" do
+
+let(:base_title){'Ruby on Rails Tutorial Sample App'}
+
 	describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -8,10 +12,18 @@ describe "StaticPages" do
     	expect(page).to have_content('Sample App')
     end
 
-    it "Should have the right title" do
-		visit '/static_pages/home'
-		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-	end
+     it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+
+
+  
   end
 
   	describe "Help page" do
@@ -22,7 +34,7 @@ describe "StaticPages" do
 		end
 		it "Should have the right title" do
 		visit '/static_pages/help'
-		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -33,9 +45,21 @@ describe "StaticPages" do
 		end
 		it "Should have the right title" do
 		visit '/static_pages/about'
-		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+		expect(page).to have_title("#{base_title} | About")
 		end
 	end
+
+	describe "Contact" do
+		it "Should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "Should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
+		end
+	end
+
 end
 
 
