742e9c8b7b81fa5ed45b94d5b54b6141ba5737a6
Pro_solvo <timqha@gmail.com>
2015-03-11 21:50:51 +0200

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ActiveRecord::Base
 	before_save {email.downcase!}
 
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+
+	has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  	has_many :followers, through: :reverse_relationships, source: :follower
+
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -27,8 +35,23 @@ class User < ActiveRecord::Base
 
 	def feed
     	# Это предварительное решение. См. полную реализацию в "Following users".
-  	  	Micropost.where("user_id = ?", id)
+  	  	#Micropost.where("user_id = ?", id)
+  	  	Micropost.from_users_followed_by(self)
+  	  	
   	end
+
+  	def following?(other_user)
+	  relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+	  relationships.create!(followed_id: other_user.id)
+	end
+
+	def unfollow!(other_user)
+	  relationships.find_by(followed_id: other_user.id).destroy!
+	end
+
 	private
 		def create_remember_token
 			self.remember_token = User.encrypt(User.new_remember_token)
